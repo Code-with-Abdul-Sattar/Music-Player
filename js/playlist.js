0091891579704d815928b8ce437aa1682b4db462
abdul sattar <juniorsattar@gmail.com>
2023-04-27 16:02:25 +0500

    Music Player

---
+++ js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "Shape Of You",
+    artist: "Ed-Sheeran",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "No Lie",
+    artist: "Sean Paul, Dua Lipa",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "Bones",
+    artist: "Imagine Dragons",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Despacito",
+    artist: "Luis Fonsi, Daddy Yankee",
+    img: "img4",
+    audio: "music4"
+  }
+]
